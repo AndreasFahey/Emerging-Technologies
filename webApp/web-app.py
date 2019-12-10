988a653b3bd6d0f80f3e0a3cc8dd6c1e9480daab
unknown <faheyandre@yahoo.co.uk>
2019-12-10 12:53:14 +0000

    Web Application Started

---
+++ webApp/web-app.py
@@ -0,0 +1,12 @@
+## Authore = Andreas Fahey
+## Student No. = G00346830
+
+## Web Application Using Flask
+
+## Imports Needed
+import flask as fl
+from keras.models import load_model
+
+## Load Model Created in Jupyter Notebook
+model = load_model('savedModel/trainedModel.h5')
+
